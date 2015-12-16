b8c5a1fe4bdbe849f3f441427da5a089bb354ed0
Shintaro-Miyazaki <shintaro.miyazaki@fhnw.ch>
2015-12-16 22:14:08 +0100

    OSC message formatting doesn't work yet

--- OSCsendtest.py
+++ OSCsendtest.py
@@ -3,24 +3,14 @@ import time, sys
 import time, random
 
 client = OSC.OSCClient()
-client.connect( ( '127.0.0.1', 57110 ) )
+client.connect( ( '127.0.0.1', 7700 ) )
 
-def oscgrain( frequency, vol, sustain ):
+def oscdmx(val):
     msg = OSC.OSCMessage()
-    msg.setAddress("s_new")
-    msg.append("grain")
-    msg.append(-1)
-    msg.append(0)
-    msg.append(1)
-    msg.append("amp")
-    msg.append(vol)
-    msg.append("freq")
-    msg.append(frequency)     #read in data points
-    msg.append("sustain")
-    msg.append(sustain)
+    msg.setAddress("/0/dmx/402")
+    msg.append(val)
     client.send(msg)
 
-oscgrain(5000, 0.8, 0.1)
-time.sleep(0.01)
-oscgrain(5000, 0.8, 0.1)
-    
+oscdmx(255)
+time.sleep(10)
+oscdmx(0)
